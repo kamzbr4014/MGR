c6da81ba9a036069fc6d4b53956732e4e81c41af
Kamil Zbroiński <kamzbr4014@gmail.com>
2021-04-20 22:56:46 +0200

    feat: add rgb split, code not synthesizable

--- MGR.tcl
+++ MGR.tcl
@@ -127,7 +127,7 @@ set_property -name "sim.central_dir" -value "$proj_dir/${_xil_proj_name_}.ip_use
 set_property -name "sim.ip.auto_export_scripts" -value "1" -objects $obj
 set_property -name "simulator_language" -value "Mixed" -objects $obj
 set_property -name "target_language" -value "VHDL" -objects $obj
-set_property -name "webtalk.xsim_launch_sim" -value "87" -objects $obj
+set_property -name "webtalk.xsim_launch_sim" -value "103" -objects $obj
 
 # Create 'sources_1' fileset (if not found)
 if {[string equal [get_filesets -quiet sources_1] ""]} {
--- hdl/dci_module.vhd
+++ hdl/dci_module.vhd
@@ -32,15 +32,67 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity dci_module is
-    Port ( pixCLK : in STD_LOGIC;
-           hSync : in STD_LOGIC;
-           vSync : in STD_LOGIC;
-           dciData : in STD_LOGIC_VECTOR (13 downto 0));
+    Port ( pixCLK   :   in STD_LOGIC;
+           hSync    :   in STD_LOGIC;
+           vSync    :   in STD_LOGIC;
+           dciData  :   in STD_LOGIC_VECTOR (7 downto 0));
 end dci_module;
 
 architecture Behavioral of dci_module is
-
+    signal vSyncActive  : std_logic := '0';
+    signal hSyncActive  : std_logic := '0';
+    signal frameSR      : std_logic_vector(15 downto 0) := (others => '0');
+    signal dataReady    : std_logic := '0';
+    
+    signal rReg         : std_logic_vector(7 downto 0) := (others => '0');
+    signal gReg         : std_logic_vector(7 downto 0) := (others => '0');
+    signal bReg         : std_logic_vector(7 downto 0) := (others => '0');
+    
 begin
+    VSyncTracker : process(vSync) -- TODO add CLK synchro
+    begin
+        if falling_edge(vSync)
+            vSyncActive <= '1';
+        else
+            vSyncActive <= '0';         
+        end if;
+    end process;
+    
+    HSyncTracker : process(hSync) -- TODO add CLK synchro
+    begin
+        if falling_edge(hSync) then
+            hSyncActive <= '1';
+        else
+            hSyncActive <= '0';         
+        end if;
+    end process;
 
-
+    DataExtraction : process(pixCLK, vSyncActive, hSyncActive)
+        variable regCount : std_logic := '0';
+    begin
+        if rising_edge(pixCLK) then
+            if vSyncActive = '1' then
+                if hSyncActive = '1' then
+                    frameSR <= frameSR(7 downto 0) & dciData;
+                    if regCount = '0' then
+                        regCount := '1';
+                        dataReady  <= '0';
+                    else
+                        regCount := '0';
+                        dataReady  <= '1';
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
+    
+    DataSegregation : process(dataReady)
+    begin
+        if dataReady = '1' then
+            rReg <= frameSR(15 downto 11) & "000";
+            gReg <= frameSR(10 downto 5)  & "000";
+            bReg <= frameSR(4  downto 0)  & "000";
+        end if;
+    end process;
+    
 end Behavioral;
