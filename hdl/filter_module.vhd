249899013410978445e0ba651ce3e615a0f5e672
Kamil Zbroiński <kamzbr4014@gmail.com>
2021-05-16 13:20:16 +0200

    feat: add direct filter shifters (without reset option)

--- hdl/filter_module.vhd
+++ hdl/filter_module.vhd
@@ -37,7 +37,7 @@ Library UNIMACRO;
 use UNIMACRO.vcomponents.all;
 
 entity filter_module is
-  Generic (W            : integer := 3;
+  Generic (W            : integer := 5;
            imgWidth     : integer := 640;
            imgHeight    : integer := 480);
   Port (pixCLK  : in std_logic;
@@ -48,37 +48,21 @@ entity filter_module is
 end filter_module;
 
 architecture Behavioral of filter_module is
---    component BRAM_ctrl_logic is
---        Generic ( isMaster  : boolean := false;
---                  imgWidth  : integer := 640;
---                  imgHeight : integer := 480);
---        Port ( CLK          : in STD_LOGIC;
---               EN           : in STD_LOGIC;
---               dataRdy      : in STD_LOGIC;
---               FRST         : in STD_LOGIC;
---               WEA          : out STD_LOGIC;
---               WEB          : out STD_LOGIC;
---               RSTA         : out STD_LOGIC;
---               RSTB         : out STD_LOGIC;
---               nCtrlEnOut   : out STD_LOGIC;
---               ADDRA        : out STD_LOGIC_VECTOR(10 downto 0);
---               ADDRB        : out STD_LOGIC_VECTOR(10 downto 0));
---    end component;
-    
-    constant numOfBRAMs     : integer := (W - 1) / 2;
+   
+    constant numOfBRAMs     : integer := ((W - 1) / 2) - 1;
     constant numOfBRAMPorts : integer := ((W - 1) * 2) - 1;
     type dataIO_t is array(numOfBRAMPorts downto 0) of std_logic_vector(7 downto 0);
     type addrBus_t is array(numOfBRAMPorts downto 0) of std_logic_vector(10 downto 0);
     subtype WEN_t is std_logic_vector(numOfBRAMPorts downto 0);
     type stdSignalarr_t     is array (numOfBRAMPorts downto 0) of std_logic;
     type stdVectSignalarr_t is array (numOfBRAMPorts downto 0) of std_logic_vector(7 downto 0);    
-    signal DOA      :  dataIO_t := (others => x"00");
-    signal DOB      :  dataIO_t := (others => x"00");
-    signal DIA      :  dataIO_t := (others => x"00");
-    signal DIB      :  dataIO_t := (others => x"00");
-    signal ADDRA    :  addrBus_t := (others => (others => '0'));
-    signal ADDRB    :  addrBus_t := (others => (others => '0'));
-    signal dataRdys      : STD_LOGIC := '0';
+    signal DOA          :  dataIO_t := (others => x"00");
+    signal DOB          :  dataIO_t := (others => x"00");
+    signal DIA          :  dataIO_t := (others => x"00");
+    signal DIB          :  dataIO_t := (others => x"00");
+    signal ADDRA        :  addrBus_t := (others => (others => '0'));
+    signal ADDRB        :  addrBus_t := (others => (others => '0'));
+    signal dataRdys     : STD_LOGIC := '0';
     signal EN           : stdSignalarr_t := (others => '0');
     signal FRST         : STD_LOGIC := '0';
     signal cntIn        : STD_LOGIC_VECTOR (9 downto 0) := (others => '0');
@@ -90,73 +74,104 @@ architecture Behavioral of filter_module is
     signal cntOut       : STD_LOGIC_VECTOR (9 downto 0) := (others => '0');
     signal FRSTOut      : STD_LOGIC := '0';
     signal nCtrlEnOut   : stdSignalarr_t :=(others => '0');
-        
+    type directShifterRow_t is array(W - 1 downto 0) of std_logic_vector(7 downto 0);
+    type directShifterArray_t is array (W - 1 downto 0) of directShifterRow_t;
+    signal directShifterArray : directShifterArray_t;
+    type filterInputs_t is array(W - 1 downto 0) of std_logic_vector(7 downto 0);
+    signal filterInputs : filterInputs_t;  
 begin
-   process(pixCLK)
-    variable sum : unsigned(7 downto 0);
-   begin
-    sum := unsigned(DOA(0)) + unsigned(DOB(0));
-    dataOut <= std_logic_vector(sum);
-   end process;
+--------- process added only for generate design ---------
+    process(pixCLK)
+        variable sum : unsigned(7 downto 0);
+    begin
+        for i in 0 to W - 1 loop
+            sum := sum + unsigned(directShifterArray(i)(W - 1));
+        end loop;
+        dataOut <= std_logic_vector(sum);
+    end process;
+----------------------------------------------------------
+    DirectShifter : process(pixCLK)
+    begin
+        if rising_edge(pixCLK) then
+            shifterLoopRow : for i in 0 to W - 1 loop
+                shifterLoopCol : for j in 0 to W - 1 loop
+                    if j = 0 then
+                        directShifterArray(i)(j) <= filterInputs(i + (j * (W-1)));
+                    else
+                        directShifterArray(i)(j) <=  directShifterArray(i)(j - 1);   
+                    end if;
+                end loop shifterLoopCol;
+            end loop shifterLoopRow;
+        end if;
+    end process; 
 
-    BRAM : BRAM_TDP_RF_module
-               port map (
-                  DOA => DOA(0),       
-                  DOB => DOB(0),      
-                  ADDRA => ADDRA(0),   
-                  ADDRB => ADDRB(0),   
-                  CLKA => pixCLK,     
-                  CLKB => pixCLK,     
-                  DIA => dataIn,       
-                  DIB => DOA(0),       
-                  ENA => '1',       
-                  ENB => '1',       
-                  RSTA => RSTA(0),     
-                  RSTB => RSTB(0),     
-                  WEA => WEA(0),      
-                  WEB => WEB(0)       
-               );
-    ctrl0 : BRAM_ctrl_logic
-               port map (CLK => pixCLK,
-                  EN => '1',
-                  dataRdy => dataRdy,
-                  FRST => FRST,
-                  WEA => WEA(0),
-                  WEB => WEB(0),
-                  RSTA => RSTA(0),
-                  RSTB => RSTB(0),
-                  ADDRA => ADDRA(0),
-                  ADDRB => ADDRB(0),
-                  nCtrlEnOut => nCtrlEnOut(0));          
---    BRAMCtrlGen : for i in 0 to numOfBRAMs generate
---            row0 : if i = 0 generate
---                    ctrl0 : BRAM_ctrl_logic
---                        port map (CLK => pixCLK,
---                          EN => '1',
---                          dataRdy => dataRdy,
---                          FRST => FRST,
---                          WEA => WEA(i),
---                          WEB => WEB(i),
---                          RSTA => RSTA(i),
---                          RSTB => RSTB(i),
---                          ADDRA => ADDRA(i),
---                          ADDRB => ADDRB(i),
---                          nCtrlEnOut => nCtrlEnOut(i));
---            end generate row0;
---            rowN : if i > 0 generate
---                    ctrlN : BRAM_ctrl_logic
---                        port map (CLK => pixCLK,
---                          EN => nCtrlEnOut(i - 1),
---                          dataRdy => dataRdy,
---                          FRST => FRST,
---                          WEA => WEA(i),
---                          WEB => WEB(i),
---                          RSTA => RSTA(i),
---                          RSTB => RSTB(i),
---                          ADDRA => ADDRA(i),
---                          ADDRB => ADDRB(i),                          
---                          nCtrlEnOut => nCtrlEnOut(i));            
---            end generate rowN;
---        end generate BRAMCtrlGen;              
-
+    BRAMGen : for i in 0 to numOfBRAMs generate
+        BRAM0 : if i = 0 generate
+            BRAM : BRAM_TDP_RF_module
+                       port map (
+                          DOA => filterInputs(i*2 + 1),      
+                          DOB => filterInputs(i*2 + 2),      
+                          ADDRA => ADDRA(i),   
+                          ADDRB => ADDRB(i),   
+                          CLKA => pixCLK,     
+                          CLKB => pixCLK,     
+                          DIA => dataIn,       
+                          DIB => filterInputs(i*2 + 1),       
+                          ENA => '1',       
+                          ENB => '1',       
+                          RSTA => RSTA(i),     
+                          RSTB => RSTB(i),     
+                          WEA => WEA(i),      
+                          WEB => WEB(i));            
+        end generate BRAM0;
+        BRAMN : if i > 0 generate
+            BRAM : BRAM_TDP_RF_module
+                       port map (
+                          DOA => filterInputs(i*2 + 1),       
+                          DOB => filterInputs(i*2 + 2),      
+                          ADDRA => ADDRA(i),   
+                          ADDRB => ADDRB(i),   
+                          CLKA => pixCLK,     
+                          CLKB => pixCLK,     
+                          DIA => filterInputs(i*2),       
+                          DIB => filterInputs(i*2 + 1),       
+                          ENA => '1',       
+                          ENB => '1',       
+                          RSTA => RSTA(i),     
+                          RSTB => RSTB(i),     
+                          WEA => WEA(i),      
+                          WEB => WEB(i));    
+        end generate BRAMN; 
+    end generate BRAMGen;
+    
+    BRAMCtrlGen : for i in 0 to numOfBRAMs generate
+            BRAMctrl0 : if i = 0 generate
+                BRAMctrl : BRAM_ctrl_logic
+                        port map (CLK => pixCLK,
+                          EN => '1',
+                          dataRdy => dataRdy,
+                          FRST => FRST,
+                          WEA => WEA(i),
+                          WEB => WEB(i),
+                          RSTA => RSTA(i),
+                          RSTB => RSTB(i),
+                          ADDRA => ADDRA(i),
+                          ADDRB => ADDRB(i),
+                          nCtrlEnOut => nCtrlEnOut(i));
+            end generate BRAMctrl0;
+            BRAMctrlN : if i > 0 generate
+                BRAMctrl : BRAM_ctrl_logic
+                        port map (CLK => pixCLK,
+                          EN => nCtrlEnOut(i - 1),
+                          dataRdy => dataRdy,
+                          FRST => FRST,
+                          WEA => WEA(i),
+                          WEB => WEB(i),
+                          RSTA => RSTA(i),
+                          RSTB => RSTB(i),
+                          ADDRA => ADDRA(i),
+                          ADDRB => ADDRB(i),                          
+                          nCtrlEnOut => nCtrlEnOut(i));            
+            end generate BRAMctrlN;
+        end generate BRAMCtrlGen;              
 end Behavioral;
