84b466dcfa65e30270ef9f0d653db7c05582e808
Kamil Zbroiński <kamzbr4014@gmail.com>
2021-05-20 21:03:34 +0200

    feat: add filter validator script

--- matlab/coeff_gen.m
+++ matlab/coeff_gen.m
@@ -8,14 +8,17 @@ coeffs = fspecial('gaussian', [W W], sigma);
 coeffsFI = ufi(coeffs, 8, fractLen);
 
 fName = 'gen/filter_coeffs.txt';
+fNameDat = 'gen/filter_coeffs.dat';
 fileID = fopen(fName, 'w');
+fileDatID = fopen(fNameDat, 'w');
 
+dlmwrite(fNameDat, coeffs);
 for i = 1 : W
     for j = 1 : W
         dataToSend = hex(coeffsFI(i,j));
         fprintf(fileID, '%s\n', dataToSend);
     end
 end
-
 fclose(fileID);
+fclose(fileDatID);
 disp("---------- File saved ----------")
